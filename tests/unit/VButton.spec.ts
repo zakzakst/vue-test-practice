7f32d7e8d82d9329ecf271e7a433f682bda75be8
zakzakst <e1433104@gmail.com>
2022-07-03 16:30:10 +0900

    ボタンのテスト

--- tests/unit/VButton.spec.ts
+++ tests/unit/VButton.spec.ts
@@ -2,28 +2,28 @@ import { shallowMount } from '@vue/test-utils';
 import VButton from '@/components/atoms/VButton.vue';
 
 // ラベル文言が反映される
-// describe('VButton.vue', () => {
-//   it('renders props.label when passed', () => {
-//     const label = 'ボタン文言';
-//     const wrapper = shallowMount(VButton, {
-//       props: { label },
-//     });
-//     expect(wrapper.text()).toMatch(label);
-//   });
-// });
+describe('VButton.vue', () => {
+  it('renders props.label when passed', () => {
+    const label = 'ボタン文言';
+    const wrapper = shallowMount(VButton, {
+      props: { label },
+    });
+    expect(wrapper.text()).toMatch(label);
+  });
+});
 
-// // disabledが反映される
-// describe('VButton.vue', () => {
-//   it('renders props.disabled when passed', () => {
-//     const wrapper = shallowMount(VButton, {
-//       props: {
-//         label: 'ボタン文言',
-//         disabled: true,
-//       },
-//     });
-//     expect(wrapper.attributes().disabled).toBe('');
-//   });
-// });
+// disabledが反映される
+describe('VButton.vue', () => {
+  it('renders props.disabled when passed', () => {
+    const wrapper = shallowMount(VButton, {
+      props: {
+        label: 'ボタン文言',
+        disabled: true,
+      },
+    });
+    expect(wrapper.attributes().disabled).toBe('');
+  });
+});
 
 // colorが反映される
 describe('VButton.vue', () => {
@@ -36,5 +36,31 @@ describe('VButton.vue', () => {
       },
     });
     expect(wrapper.classes()).toContain(`--color-${color}`);
+  });
+});
+
+// roundedが反映される
+describe('VButton.vue', () => {
+  it('renders props.rounded when passed', () => {
+    const wrapper = shallowMount(VButton, {
+      props: {
+        label: 'ボタン文言',
+        rounded: true,
+      },
+    });
+    expect(wrapper.classes()).toContain('rounded-full');
+  });
+});
+
+// emit clickが実行される
+describe('VButton.vue', () => {
+  it('emit click', () => {
+    const wrapper = shallowMount(VButton, {
+      props: {
+        label: 'ボタン文言',
+      },
+    });
+    wrapper.vm.$emit('click');
+    expect(wrapper.emitted().click.length).toBe(1);
   });
 });
