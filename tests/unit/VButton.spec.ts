748932ce09657de5b59b3d4520db616f372157a4
zakzakst <e1433104@gmail.com>
2022-07-03 16:19:57 +0900

    ボタンのテスト作成

---
+++ tests/unit/VButton.spec.ts
@@ -0,0 +1,40 @@
+import { shallowMount } from '@vue/test-utils';
+import VButton from '@/components/atoms/VButton.vue';
+
+// ラベル文言が反映される
+// describe('VButton.vue', () => {
+//   it('renders props.label when passed', () => {
+//     const label = 'ボタン文言';
+//     const wrapper = shallowMount(VButton, {
+//       props: { label },
+//     });
+//     expect(wrapper.text()).toMatch(label);
+//   });
+// });
+
+// // disabledが反映される
+// describe('VButton.vue', () => {
+//   it('renders props.disabled when passed', () => {
+//     const wrapper = shallowMount(VButton, {
+//       props: {
+//         label: 'ボタン文言',
+//         disabled: true,
+//       },
+//     });
+//     expect(wrapper.attributes().disabled).toBe('');
+//   });
+// });
+
+// colorが反映される
+describe('VButton.vue', () => {
+  it('renders props.color when passed', () => {
+    const color = 'primary';
+    const wrapper = shallowMount(VButton, {
+      props: {
+        label: 'ボタン文言',
+        color,
+      },
+    });
+    expect(wrapper.classes()).toContain(`--color-${color}`);
+  });
+});
